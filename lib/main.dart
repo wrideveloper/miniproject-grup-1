ec35c67f9a3da51ef161e1bf6d7dcdd3e7568eda
riVFerd <virgyferdian213@gmail.com>
2022-01-15 23:43:26 +0700

    Membuat start page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wrigroup1/pages/start_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      // home: HalamanKalian(),
+      home: const SplashScreen(),
     );
   }
 }
---
+++ lib/pages/start_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xff333333),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            "assets/SplashImages.png",
+            width: 500,
+            height: 500,
+            fit: BoxFit.fill,
+          ),
+          const SizedBox(height: 30),
+          const Text(
+            'Let\'s improve your appearence',
+            style: TextStyle(
+                fontFamily: "Arial",
+                fontWeight: FontWeight.normal,
+                fontSize: 35,
+                color: Colors.white,
+                decoration: TextDecoration.none),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 15),
+          const Text(
+            'find cool shoes to support your daily activities',
+            style: TextStyle(
+                fontFamily: "Arial",
+                fontSize: 15,
+                color: Colors.white,
+                fontWeight: FontWeight.normal,
+                decoration: TextDecoration.none),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          GestureDetector(
+            onTap: () {
+              //Widget dari home_page.dart
+            },
+            child: Container(
+              alignment: Alignment.center,
+              width: 220,
+              height: 60,
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              child: const Text(
+                'Get Started',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: "Arial",
+                    fontWeight: FontWeight.normal,
+                    fontSize: 25,
+                    decoration: TextDecoration.none),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
